3ae5c511b5f77ee2d496712996d3433ad36947a0
Wangondu, Mercy Mumbi <132012844+MercyWangondu@users.noreply.github.com>
2026-02-01 11:31:07 +0300

    Fixing the order items sql query
    Addition of missing columns

--- sql/01_create_tables.sql
+++ sql/01_create_tables.sql
@@ -25,6 +25,8 @@ CREATE TABLE order_items (
     order_id VARCHAR,
     order_item_id INT,
     product_id VARCHAR,
+    seller_id VARCHAR,
+    shipping_limit_date TIMESTAMP,
     price NUMERIC,
     freight_value NUMERIC,
     PRIMARY KEY (order_id, order_item_id)
